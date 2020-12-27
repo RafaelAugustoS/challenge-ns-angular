e9c20d6d01944875ee60b8dc870953172653e36a
Rafael Augusto <rafaelaugusto.developer@gmail.com>
2020-12-27 17:45:57 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core'
+import { NativeScriptModule, NativeScriptFormsModule } from '@nativescript/angular'
+
+import { AppComponent } from './app.component'
+import { CurrentChallengeComponent } from './challenges/current-challenge/current-challenge.component'
+import { StackComponent } from './layouts/stack/stack.component'
+
+@NgModule({
+	bootstrap: [AppComponent],
+	imports: [NativeScriptModule, NativeScriptFormsModule],
+	declarations: [AppComponent, CurrentChallengeComponent, StackComponent],
+	providers: [],
+	schemas: [NO_ERRORS_SCHEMA],
+})
+export class AppModule {}
